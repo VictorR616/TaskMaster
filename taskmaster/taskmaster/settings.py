947898573a9323d854682b74801825e58aebf7a1
Victor616 <victor616rojas@gmail.com>
2023-09-12 17:36:57 -0300

    feat(UI): Added CSS Style to list tasks and create tasks

--- taskmaster/taskmaster/settings.py
+++ taskmaster/taskmaster/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Ruta de archivos estaticos
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todo_task', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
