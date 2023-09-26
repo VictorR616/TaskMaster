fa7472872c2051e8077cfaec3b2f2a7cf0a44f65
Victor616 <victor616rojas@gmail.com>
2023-09-26 16:53:24 -0300

    fix(UI): Make some responsive design on tables

--- taskmaster/taskmaster/static/js/main.js
+++ taskmaster/taskmaster/static/js/main.js
@@ -1,13 +1,31 @@
-// JavaScript para controlar la visibilidad del menú desplegable
 document.addEventListener("DOMContentLoaded", function () {
+    // Elementos DOM
     const dropdownButton = document.getElementById("dropdown-button");
     const dropdownContent = document.getElementById("myDropdown");
+    const table = document.querySelector(".table");
+    const cardContainer = document.querySelector(".card-container");
 
+    // Función para cambiar la vista según el ancho de la pantalla
+    function toggleView() {
+        if (window.innerWidth <= 900) {
+            table.style.display = "none";
+            cardContainer.style.display = "flex";
+        } else {
+            table.style.display = "table";
+            cardContainer.style.display = "none";
+        }
+    }
+
+    // Evento de carga inicial y cambio de tamaño de ventana
+    toggleView();
+    window.addEventListener("resize", toggleView);
+
+    // Evento de clic en el botón desplegable
     dropdownButton.addEventListener("click", function () {
         dropdownContent.classList.toggle("show");
     });
 
-    // Cierra el menú si se hace clic fuera de él
+    // Evento para cerrar el menú al hacer clic fuera de él
     window.addEventListener("click", function (event) {
         if (!event.target.matches("#dropdown-button")) {
             const dropdowns = document.getElementsByClassName("dropdown-content");
